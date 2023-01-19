5b31328af8e4c12a49aaef377878006b22a2259c
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-01-19 18:45:15 -0300

    feat: Cria to GET /api-docs e arquivos para documentação swagger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 import httpErrorMiddleware from './middlewares/http.error.middleware';
 import productsRoutes from './routes/products.routes';
 import usersRoutes from './routes/users.routes';
 import ordersRoutes from './routes/orders.routes';
 import loginRoutes from './routes/login.routes';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
@@ -14,6 +16,7 @@ app.use('/products', productsRoutes);
 app.use('/users', usersRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/login', loginRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(httpErrorMiddleware);
 
